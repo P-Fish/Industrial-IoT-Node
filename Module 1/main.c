934dc61c175c77c09f69de906a42ca53a6a2b413
peter <peterfish25@gmail.com>
2025-03-19 22:36:54 -0700

    Ready to test Module 1 HW

--- Module 1/main.c
+++ Module 1/main.c
@@ -1,12 +1,34 @@
+#include<uart.h>
+
 /**********************************************************
 * main.c
 *
 *
 */
+void writestr(char *str);
 
 int main() {
-    while (1) {
+    uart_init();
 
-    }
+    writestr("Hello World from Atmega328");
+    writestr("SER486 -- Homework Assignment 1");
+    writestr("Peter Fischbach");
+    writestr("char size = %d bits", sizeof(char));
+    writestr("int size = %d bits", sizeof(int));
+    writestr("long size = %d bits", sizeof(long));
+
+    while (1) {}
     return 0;
+}
+
+void writestr(char *str) {
+    
+    unsigned int ind = 0;
+    
+    while (*(str + ind) != 0 )
+    {
+        uart_writechar(*(str + ind++));
+    }
+
+    return;
 }
